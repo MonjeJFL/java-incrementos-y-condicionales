c9882535ebee0c57859118e848e1d2a6c719bf37
MonjeJFL <u20261244865@usco.edu.co>
2026-08-27 16:19:13 -0500

    Actualizar todas las carpetas y contenido

--- src/Ejercicio3Transporte.java
+++ src/Ejercicio3Transporte.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
 /**
  *
- * @author Usuario
+ * @Jose Fernando Losada Monje
  */
 public class Ejercicio3Transporte {
+    public static void main(String[] args) {
+        double cal, cv;
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Cual es su Calificacion: ");
+        cal=sc.nextDouble();
+        System.out.println("Cual es su cantidad de viajes: ");
+        cv=sc.nextDouble();
+        System.out.println("Cual es su disponibilidad (Disponible/Ocupado): ");
+        String disp=sc.next();
+        //condicion en la que todas son correctas
+        if (cal>=48 && cv>=500 && disp.equalsIgnoreCase("Disponible")){
+            System.out.println("Usted fue asignado como conductor premium");
+        }
+        //condicion en la que minimamente 2 son correctas usando el "ó" que se escribe como "||"
+        else if (cal>=48 && cv>=500 && disp.equalsIgnoreCase("Ocupado")||cal<48 && cv>500 && disp.equalsIgnoreCase("Disponible")||cal>=48 && cv<500 && disp.equalsIgnoreCase("Disponible")){
+             System.out.println("Usted fue asignado como conductor standar");
+        }
+        //condicion en la cual ninguna es correcta
+        else{ 
+            System.out.println("No fue aprovado como conductor");
+        }
+        
+    }
     
 }
