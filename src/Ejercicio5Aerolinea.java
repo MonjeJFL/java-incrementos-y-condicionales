2b47d1a1e4f72e3548e0afb8ff8ead7bd386e1dd
MonjeJFL <u20261244865@usco.edu.co>
2026-08-27 17:44:45 -0500

    Actualizar todos los archivos

--- src/Ejercicio5Aerolinea.java
+++ src/Ejercicio5Aerolinea.java
@@ -1,12 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
+import java.util.Scanner;
 /**
  *
- * @author Usuario
+ * @Jose Fernando Losada Monje
  */
 public class Ejercicio5Aerolinea {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        
+       int vuelos;
+       
+        System.out.println("Posee membresia (Si/No): ");
+        String membresia=sc.next();
+        System.out.println("Cual es su cantidad de vuelos realizados: ");
+        vuelos=sc.nextInt();
+        
+        if (vuelos>=50 && vuelos<=70 && membresia.equalsIgnoreCase("Si")){
+            System.out.println("Su categoria es Oro");
+            
+        }if (vuelos>70 && membresia.equalsIgnoreCase("Si")){
+                System.out.println("Su categoria es Oro con acceso VIP");
+            }
+        else if (vuelos<50 && vuelos>=20 && membresia.equalsIgnoreCase("Si")){
+            System.out.println("Su categoria es Plata");
+        }
+        else if (vuelos<20 || membresia.equalsIgnoreCase("No")){
+            System.out.println("Su categoria es Basica");
+        } 
+    }   
+}      
+        
+        
+        
     
-}
+
