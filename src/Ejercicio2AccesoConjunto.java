2dfaac57a23fc0166db57809c467dccb268a0302
MonjeJFL <u20261244865@usco.edu.co>
2026-08-27 15:36:39 -0500

    Agregar todas las actualizaciones

--- src/Ejercicio2AccesoConjunto.java
+++ src/Ejercicio2AccesoConjunto.java
@@ -1,12 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
 /**
  *
- * @author Usuario
+ * @Jose Fernando Losada Monje
  */
 public class Ejercicio2AccesoConjunto {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tiene tarjeta de acceso (Si/No): ");
+        String tienetarjeta = sc.next();
+
+        if (tienetarjeta.equalsIgnoreCase("Si")) {
+            System.out.println("La tarjeta sigue activa (Si/No): ");
+            String tarjetaactiva = sc.next();
+
+            if (tarjetaactiva.equalsIgnoreCase("Si")) {
+                System.out.println("Tiene deudas (Si/No): ");
+                String tarjetadeudas = sc.next();
+
+                if (tarjetadeudas.equalsIgnoreCase("No")) {
+                    System.out.println("Acceso Permitido");
+                } else {
+                    System.out.println("Acceso Restringido por deudas");
+                }
+
+            } else {
+                System.out.println("Acceso Restringido por tarjeta inactiva");
+            }
+
+        } else {
+            System.out.println("Acceso restringido ausencia de tarjeta");
+        }
+    }
 }
+ 
+    
+
+ 
