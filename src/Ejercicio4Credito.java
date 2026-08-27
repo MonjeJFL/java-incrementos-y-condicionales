7af3a125abd6319161895e523a68d69b6472200c
MonjeJFL <u20261244865@usco.edu.co>
2026-08-27 17:22:49 -0500

    actualizar todos los documentos

--- src/Ejercicio4Credito.java
+++ src/Ejercicio4Credito.java
@@ -1,12 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
+
+
 
 /**
  *
- * @author Usuario
+ * @Jose Fernando Losada Monje
  */
 public class Ejercicio4Credito {
+    public static void main(String[] args) {
+        int pc,tm;
+        double ing;
+        String nombre,telefono,cedula;
+        Scanner sc= new Scanner (System.in);
+        
+        System.out.println("Ingrese su nombre completo: ");
+        nombre=sc.nextLine();
+        System.out.println("Ingrese su cedula: ");
+        cedula=sc.next();
+        System.out.println("Ingrese su numero de telefono: ");
+        telefono=sc.next();
+        
+        System.out.println("");
+        
+        System.out.println("Ingrese su cantidad de ingresos mensuales: ");
+        ing=sc.nextDouble();
+        
+        System.out.println("Ingrese su puntaje crediticio: ");
+        pc=sc.nextInt();
+        
+        System.out.println("Ingrese la cantidad de tiempo en meses de su actual trabajo: ");
+        tm=sc.nextInt();
+        
+        System.out.println("");
+        System.out.println("El solicitante "+nombre+" con cedula de ciudadania "+cedula+" y numero de telefono "+telefono);
+        
+        if (ing>5000000 && pc>750 && tm>24){
+            System.out.println("fue aprovado para su credito");
+         }
+        else if (ing>500000 && pc>750 && tm<24 || ing>500000 && pc<=750 && tm>24 || ing<=5000000 && pc>750 && tm>24){
+                 System.out.println("fue aprovado para un credito condicionado");
+         } else {
+            System.out.println("fue rechazado para su credito");
+        }
+        }
+}  
+        
+      
+        
     
-}
+    
+
